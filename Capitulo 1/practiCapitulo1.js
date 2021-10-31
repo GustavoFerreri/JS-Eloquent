4ea777010436ee9d99597fef3c5e0b04db9dde0a
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2021-10-31 09:21:52 -0300

    Finalizacion del capitulo 1

--- Capitulo 1/practiCapitulo1.js
+++ Capitulo 1/practiCapitulo1.js
@@ -16,3 +16,43 @@ console.log(true && true);
 console.log(true || false);
 console.log(2<3);
 console.log(2>3);
+
+// Por ultimo el operador ternario
+// O condicional
+console.log(`\nOperador ternario`);
+console.log(2>3 ? 'Es mayor' : 'Es menor');
+console.log(true? true : false);
+console.log(true? 1 : 2);
+
+// operadores null o indefinidos, conversion automatica por parte de javascript
+console.log(`\nOperadores null o indefinidos`);
+console.log(null==undefined);
+console.log(null!=undefined);
+console.log(''==0);
+console.log(''==false);
+console.log(''==null);
+
+// Operador de corto circuito
+console.log(`\nOperador de corto circuito`);
+console.log(true && console.log('Hola mundo'));
+console.log(null||'Hola');
+console.log('hola'&&'');
+
+// Resumen del capitulo
+// 4 tipos de valores
+//      - String
+//      - Number
+//      - Boolean
+//      - Valores indefinidos
+// No se asigna el tipo si no es detectado por el mismo javascript
+// Vimos operadores
+//     - Aritmeticos
+//     - Logico
+//     - Relacional
+//     - Asignacion
+//     - Concatenacion
+//     - Operadores unarios
+//     - Operadores binarios
+//     - Operadores ternarios
+//     - Operadores de corto circuito
+//     - Operadores null o indefinidos
